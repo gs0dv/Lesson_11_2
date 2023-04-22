827cb78ad8d9cc24d9daa898b63ebd4fd059d070
gs0dv <gs0dv@main.ru>
2023-04-22 22:51:23 +0300

    added function upper_first_ch in file str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def all_ch_upper(_str: str) -> str:
     return _str.upper()
+
+
+def upper_first_ch(_str: str) -> str:
+    """
+    Возвращает строку с заглавными первыми буквами каждого слова
+    :param _str: str
+    :return: str
+    """
+    return _str.title()
